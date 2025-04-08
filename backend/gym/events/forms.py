467ba8bce9d1de54138dbf99e33f48b98e5ae0a0
Dxtag <dxtagp+github@gmail.com>
2025-04-08 21:05:47 +0200

    New Tests, check for overlapping event dates

--- backend/gym/events/forms.py
+++ backend/gym/events/forms.py
@@ -3,13 +3,24 @@ from .models import EventDate
 from django.utils.timezone import datetime
 from datetime import time
 
-# TODO 
-# - check for overlapping event dates []
 
 class EventDateForm(forms.ModelForm):
     class Meta:
         fields = "__all__"
         model=EventDate
+
+
+    def clean(self):
+        new_start_time:str = self.cleaned_data["start_time"]
+        new_end_time  :str = self.cleaned_data["end_time"]
+        other_dates = EventDate.objects.filter(weekday=self.cleaned_data["weekday"])
+        for date in other_dates:
+            starts_inside = date.start_time < new_start_time and date.end_time > new_start_time
+            ends_inside = date.start_time < new_end_time and date.end_time > new_end_time
+            is_duplicate = date.start_time == new_start_time and date.end_time == new_end_time
+            if starts_inside or ends_inside or is_duplicate:
+                raise forms.ValidationError(f"This date is overlapping with event(pk={date.pk})")
+        return super().clean()
 
 
 
--- backend/gym/events/tests.py
+++ backend/gym/events/tests.py
@@ -34,39 +34,68 @@ class EventListApiViewTestCase(APITestCase):
             self.assertIn("start_time", response.data[0]["dates"][0])
             self.assertIn("end_time", response.data[0]["dates"][0])
 
-class EventDateFormTest(TestCase):
+
+class EventDateFormTests(TestCase):
     def setUp(self):
         event_type = EventType.objects.create(name="Powerlifting", description="Powerlifting event")
         self.room = Room.objects.create(number=101, name="Small Room")
         coach = Coach.objects.create(firstname="John", lastname="Doe")
+        user = get_user_model().objects.create_user(username="testuser", password="testpassword")
+        
         self.event = Event.objects.create(
             event_type=event_type,
             coach=coach,
             person_limit=20,
         )
+
         self.existing_event = EventDate.objects.create(
-            event=self.event, weekday='Monday', start_time=time(10, 0), end_time=time(12, 0)
+            event = self.event,
+            weekday='Mon',
+            start_time=time(10, 0),
+            end_time=time(12, 0),
+            room=self.room
         )
 
+    def test_duplicate_event(self):
+        form = EventDateForm(data={
+            'event' : self.event,
+            'room' : self.room,
+            'weekday': 'Mon',
+            'start_time': time(10, 0),
+            'end_time': time(12, 0),
+        })
+        self.assertFalse(form.is_valid())
+        self.assertIn('This date is overlapping', str(form.errors))
 
-    def test_valid_event(self):
-        form_data = {"event":self.event,"weekday": "Mon", "start_time": time(12, 30), "end_time": time(14, 0), "room":self.room}
-        form = EventDateForm(data=form_data)
+    def test_overlapping_start_time(self):
+        form = EventDateForm(data={
+            'event' : self.event,
+            'room' : self.room,
+            'weekday': 'Mon',
+            'start_time': time(11, 0),
+            'end_time': time(13, 0),
+        })
+        self.assertFalse(form.is_valid())
+        self.assertIn('This date is overlapping', str(form.errors))
+
+    def test_overlapping_end_time(self):
+        form = EventDateForm(data={
+            'event' : self.event,
+            'room' : self.room,
+            'weekday': 'Mon',
+            'start_time': time(9, 0),
+            'end_time': time(11, 0),
+        })
+        self.assertFalse(form.is_valid())
+        self.assertIn('This date is overlapping', str(form.errors))
+
+    def test_non_overlapping_event(self):
+        form = EventDateForm(data={
+            'event' : self.event,
+            'room' : self.room,
+            'weekday': 'Mon',
+            'start_time': time(20, 0),
+            'end_time': time(21, 0),
+        })
         self.assertTrue(form.is_valid())
-
-
-    def test_overlapping_event(self):
-        form_data = {"event":self.event,"weekday": "Mon", "start_time": time(11, 0), "end_time": time(13, 0), "room":self.room}
-        form = EventDateForm(data=form_data)
-        self.assertFalse(form.is_valid())
-
-    def test_invalid_time_order(self):
-        form_data = {"event":self.event,"weekday": "Mon", "start_time": time(14, 0), "end_time": time(13, 0), "room":self.room}
-        form = EventDateForm(data=form_data)
-        self.assertFalse(form.is_valid())
-
-    def test_exact_match(self):
-        form_data = {"event":self.event,"weekday": "Mon", "start_time": time(10, 0), "end_time": time(12, 0), "room":self.room}
-        form = EventDateForm(data=form_data)
-        self.assertFalse(form.is_valid())
 
